fa041a121859e1b1e34f4730263eaa4509d10a67
naylatest01 <82061519+naylatest01@users.noreply.github.com>
2022-03-18 01:52:44 +0000

    Implemented buttonSubmitClick

--- src/index.ts
+++ src/index.ts
@@ -3,26 +3,33 @@ import isPalindrome from './checker';
 
 console.log("index.ts/start");
 
-//Obtener referencia a inputText
-const inputText = <HTMLInputElement>document.getElementById('inputText');
-let bPalindrome = null;
-if (inputText) {
-    //Invocar isPalindrome
-    bPalindrome = isPalindrome(inputText.value);
-    console.log(bPalindrome);
-}
+function buttonSubmitClick() {
+    //Obtener referencia a inputText
+    const inputText = <HTMLInputElement>document.getElementById('inputText');
+    let bPalindrome = null;
+    if (inputText) {
+        //Invocar isPalindrome
+        bPalindrome = isPalindrome(inputText.value);
+        console.log(bPalindrome);
+    }
 
-//Actualizar divResult
-const divResult = <HTMLDivElement>document.getElementById('divResult');
-if (divResult) {
-    if (bPalindrome) {
-        divResult.innerText = 'The word is palindrome!'
-        divResult.className = "palindrome";
-    } else {
-        divResult.innerText = 'The word is not palindrome, sorry.'
-        divResult.className = "notPalindrome";
+    //Actualizar divResult
+    const divResult = <HTMLDivElement>document.getElementById('divResult');
+    if (divResult) {
+        if (bPalindrome) {
+            divResult.innerText = 'The word is palindrome!'
+            divResult.className = "palindrome";
+        } else {
+            divResult.innerText = 'The word is not palindrome, sorry.'
+            divResult.className = "notPalindrome";
+        }
     }
 }
 
+const btn = document.querySelector('button');
+if (btn) {
+  console.log('attaching listener');
+  btn.addEventListener('click', buttonSubmitClick);
+}
 
 
